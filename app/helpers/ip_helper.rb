6fafdf50a22cac0684452e86f8967aba96669b97
Aleksander Monteiro <aleksmont@gmail.com>
2025-04-11 15:26:02 -0300

    ip

--- app/helpers/ip_helper.rb
+++ app/helpers/ip_helper.rb
@@ -1,13 +1,16 @@
+require 'net/http'
+require 'net/https'
+
 module IpHelper
   TOKEN = '6b89820d302b80'
 
   def self.get_info(ip_address)
     begin
       url = URI("https://ipinfo.io/#{ip_address}?token=#{TOKEN}")
-      res = Net::HTTP.get(url)
-      resp = JSON.parse(res)
+      response = Net::HTTP.get_response(url)
+      resp = JSON.parse(response.body)
       {
-        success: resp['city'].present?,
+        success: response.code == 200,
         city: resp["city"],
         state: resp["region"],
         country: resp["country"]
