f2bbe17ae9a726746f6826b1361d8ba5233064b4
Aleksander Monteiro <aleksmont@gmail.com>
2025-04-11 14:27:10 -0300

    bootstrap styles

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -5,34 +5,34 @@ class AuthController < ApplicationController
 
   def authenticate
     begin
-      user = User.find_by(email: params[:email])
+      user = User.find_by(email: authenticate_params[:email])
 
-      if user.authenticate(params[:password])
+      if user.authenticate(authenticate_params[:password])
         # revoke all other sessions
         revoke_sessions(user.id)
 
         ip_address = request.remote_ip
         user_session = UserSession.new(user_id: user.id, ip_address: ip_address)
 
-        # Geocodificar o IP
-        location = Geocoder.search(@ip).first
+        ip_info = IpHelper::get_info(ip_address)
 
-        if location
-          user_session.city = location.city
-          user_session.state = location.state
-          user_session.country = location.country
+        if ip_info[:success]
+          user_session.city = ip_info[:city]
+          user_session.state = ip_info[:state]
+          user_session.country = ip_info[:country]
         end
 
-        user_session.save
+        user_session.save!
 
-        session[:current_user_id] = @user.id
+        session[:current_user_id] = user.id
 
         redirect_to root_path, notice: "Login was successfully."
       else
-        redirect_to root_path, notice: "Login unsuccessful."
+        flash[:alert] = "Password is incorrect."
+        redirect_to '/auth/login'
       end
     rescue StandardError => e
-      redirect_to root_path, notice: "Login unsuccessful."
+      redirect_to '/auth/login', notice: "Login unsuccessful."
     end
   end
 
@@ -40,13 +40,17 @@ class AuthController < ApplicationController
     begin
       revoke_sessions(session[:current_user_id])
       session[:current_user_id] = nil
-      redirect_to root_path, notice: "Logout successful."
+      redirect_to '/auth/login', notice: "Logout successful."
     rescue StandardError => e
-      redirect_to action: "new", notice: "Session not found."
+      redirect_to action: '/auth/login', notice: "Session not found."
     end
   end
 
   def revoke_sessions(user_id)
     UserSession.where(user_id: user_id).update(revoked: true)
+  end
+
+  def authenticate_params
+    params.require(:session).permit(:email, :password)
   end
 end
